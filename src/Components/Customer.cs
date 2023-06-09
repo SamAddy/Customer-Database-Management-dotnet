b27c00de408ac4c8a2a142068f58fa39d5506686
SamAddy <addisonsamuel70@yahoo.com>
2023-06-09 15:28:23 +0300

    Update customer class with address property

--- src/Components/Customer.cs
+++ src/Components/Customer.cs
@@ -1,3 +1,4 @@
+namespace Component.CustomersDatabase;
 class Customer 
 {
     private static int idCounter = 1;
@@ -5,12 +6,14 @@ class Customer
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+    public string Address { get; set; }
 
-    public Customer(string firstName, string lastName, string email)
+    public Customer(string firstName, string lastName, string email, string address)
     {
         FirstName = firstName;
         LastName = lastName;
         Email = email;
+        Address = address;
         _id = GenerateId();
     }
     private string GenerateId()
@@ -22,6 +25,6 @@ class Customer
 
     public override string ToString()
     {
-        return $"{_id} First Name: {FirstName} Last Name: {LastName} Email: {Email}";
+        return $"{_id} \n Name: {FirstName}{LastName}\n Email: {Email}\n Address: {Address}\n";
     }
 }
