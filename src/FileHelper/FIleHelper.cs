8d29a08963550615e974c29d83f0c707ef6af59a
SamAddy <addisonsamuel70@yahoo.com>
2023-06-09 19:05:20 +0300

    Add file helper class

---
+++ src/FileHelper/FIleHelper.cs
@@ -0,0 +1,47 @@
+namespace Component.CustomersDatabase;
+
+public class FileHelper
+{
+    private string _filePath;
+    private FileInfo? _fileInfo;
+
+    public FileHelper(string filepath)
+    {
+        if (string.IsNullOrWhiteSpace(filepath))
+            throw new ArgumentException("File path cannot be empty or null.");
+
+        _filePath = filepath;
+        _fileInfo = new FileInfo(_filePath);
+
+        if (!_fileInfo.Exists)
+            throw new FileNotFoundException("File not found.", _filePath);
+    }
+    public string[]? GetAll()
+    {
+        try
+        {
+            var data = File.ReadAllLines(_filePath);
+            return data;
+        }
+        catch (IOException e)
+        {
+            throw new FileNotFoundException("An error occurred while reading the file.", e);
+        }
+    }
+
+    public void WriteToFile(string content)
+    {
+        File.AppendAllText(_filePath, content);
+    }
+
+    public class FileHelperException : Exception
+    {
+        public FileHelperException(string message) : base(message)
+        {
+        }
+
+        public FileHelperException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
