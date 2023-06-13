366d565dff38fa27163fd70966f7f0e86b914f89
SamAddy <addisonsamuel70@yahoo.com>
2023-06-13 16:19:43 +0300

    test features

--- src/Program.cs
+++ src/Program.cs
@@ -8,15 +8,15 @@ internal class Program
 
         Customer customer1 = new Customer("Customer-1", "John", "Doe", "john@example.com", "123 Main St");
         Customer customer2 = new Customer("Customer-2", "Jane", "Smith", "jane@example.com", "456 Elm St");
+        Customer customer3 = new Customer("Customer-3", "Jelly", "Smith", "jelly@example.com", "321 Wall St");
         Customer updated1 = new Customer("Customer-2", "Ellen", "Doe", "newemail@example.com", "789 Oak St");
         string customerId = "Customer-1";
-        Console.WriteLine(customer1);
+
         customerDatabase.AddCustomer(customer1);
         customerDatabase.AddCustomer(customer2);
-        customerDatabase.UpdateCustomer(updated1);
+        // customerDatabase.UpdateCustomer(updated1);
         customerDatabase.GetCustomerById(customerId);
-        
-        // customerDatabase.AddCustomer(customer2);
-        // // Console.WriteLine(customerDatabase);
+        customerDatabase.DeleteCustomer(customerId);
+        // customerDatabase.AddCustomer(customer3);
     }
 }
